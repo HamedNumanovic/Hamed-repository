a1b914287f121c0c876a375a23c8237f91e5a446
Hamed Numanović <hamednumanovic1@gmail.com>
2021-10-26 21:06:41 +0200

    commit

---
+++ Java 4/app.js
@@ -0,0 +1,47 @@
+// var1 = ["Banana", "Mango", "Apple", 320]
+
+// var2 = var1.join("-")
+
+// console.log(var1)
+
+// console.log(var2)
+
+// var1 = ["Hamed", "Samid", "Furkan", "Umejr"]
+
+// console.log(var1)
+
+// vrednostkojasevraca = var1.pop();
+
+// console.log(var1)
+
+// console.log(vrednostkojasevraca)
+
+// var1 = ["Hamed", "Samid", "Furkan", "Umejr"]
+
+// console.log(var1)
+
+// vrednostkojasevraca = var1.shift();
+
+// console.log(var1)
+
+// console.log(vrednostkojasevraca)
+
+// var1 = ["Hamed", "Samid", "Furkan", "Umejr"]
+
+// console.log(var1)
+
+// vrednostkojasevraca = var1.unshift("JavaScript");
+
+// console.log(var1)
+
+// console.log(vrednostkojasevraca)
+
+// DA PROCITAMO DO SPLICING AND ARRAY
+
+var1 = ["Hamed", "Samid", "Furkan", "Umejr"]
+
+console.log(var1)
+
+delete var1[1]
+
+console.log(var1)
